262003fed6bfdd5945e2b84d538acc468677dec8
Rick <yishilin@gmail.com>
2017-11-09 18:57:11 +0800

    v1.0: to send the trello notification via dingtalk work notification

--- app/trello/handler.js
+++ app/trello/handler.js
@@ -105,7 +105,8 @@ function Handler(api, action, view_root, dingtalk_tokens) {
                     let send2dingtalk = require('./sender.js');
                     send2dingtalk(dingMsgJson, dingtalk_tokens);
 
-                    handler_placeholder.api.asyncsend_notification(dingMsgJson.markdown);
+                    let ding_userids = get_dinguserids_by(dingMsgJson.at.atMobiles);
+                    handler_placeholder.api.asyncsend_notification(dingMsgJson.markdown, ding_userids);
                 });
             }
         } else {
@@ -174,6 +175,61 @@ function Handler(api, action, view_root, dingtalk_tokens) {
 
         return action.display.translationKey;
     }
+}
+
+function get_dinguserids_by(target_mobiles) {
+    /*
+     API.api.userList({department_id: 40641196}, function(err, res) {
+        ding_users_in_dep = res.userlist.map(function(x) { return {
+            "mobile": x.mobile,
+            "name": x.name,
+            "userid": x.userid 
+        }; }); 
+    })
+    */
+    
+    ding_users_in_dep = [ 
+      { mobile: '15101155482', name: '柯钢', userid: 'kegang' },
+      { mobile: '18600427612', name: '张雅峰', userid: 'zhangyfk' },
+      { mobile: '18910562597', name: '赵刚刚', userid: 'zhaogg' },
+      { mobile: '18910562544',
+        name: '赵苗苗',
+        userid: '066933530335872533' },
+      { mobile: '15010283641',
+        name: '张春艳',
+        userid: '112210682324246286' },
+      { mobile: '15801672342',
+        name: '俞丽丽',
+        userid: '015315424320289534' },
+      { mobile: '13301189821',
+        name: '易石林',
+        userid: '014216536726090327' },
+      { mobile: '18656959808',
+        name: '王任翔',
+        userid: '010405294529084932' },
+      { mobile: '13681587146',
+        name: '宋方华',
+        userid: '066933222523349632' },
+      { mobile: '18910562723',
+        name: '穆琳琳',
+        userid: '066864444431033126' },
+      { mobile: '15801150718', name: '吕炎', userid: '0669406738696089' },
+      { mobile: '18910562760',
+        name: '刘木春',
+        userid: '066866092821041173' },
+      { mobile: '18801488690', name: '李帅', userid: 'lishuaic' },
+      { mobile: '18656959890', name: '梁亮', userid: '0669553801849485' },
+      { mobile: '15840211528', name: '何萍', userid: '0669400050663384' },
+      { mobile: '13683279203',
+        name: '郭晓伟',
+        userid: '085820564636486361' },
+      { mobile: '13693537625',
+        name: '陈娟',
+        userid: '11221336221215703' } ]
+
+    ts = ding_users_in_dep.filter(function(x){ return target_mobiles.indexOf(x.mobile) > -1 });
+    ding_userids = ts.map(function(x) { return x.userid; }); 
+    return ding_userids; 
 }
 
 module.exports = Handler;
